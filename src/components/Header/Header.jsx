7084735ae5aabf99f22b80e7a256b5740b622ad8
Vladyslav Mykhalytskyi <vmykhali666@gmail.com>
2024-06-10 00:08:34 +0300

    hero

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,12 +2,13 @@ import Logo from '../ui/Logo';
 import AuthBar from './AuthBar';
 import Navigation from './Navigation';
 import UserBar from './UserBar';
+import cl from './header.module.scss';
 
 const Header = () => {
   const isUserAuthorized = false;
 
   return (
-    <header>
+    <header className={cl.header}>
       <Logo />
 
       {isUserAuthorized ? (
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,21 @@
+import Button from '../ui/Button';
 import cl from './hero.module.scss';
 
 const Hero = () => {
-  return <div className={cl.className}>Hero</div>;
+  return (
+    <section className={cl.section}>
+      <h1 className={cl.title}>Improve Your<br/>Culinary Talents</h1>
+      <p className={cl.desc}>
+        Amazing recipes for beginners in the world of cooking, enveloping you in
+        the aromas and tastes of various cuisines.
+      </p>
+      <Button className={cl.button}>Add Recipe</Button>
+      <div className={cl.imgContainer}>
+        <img src="/public/images/categories/Breakfast.jpg" alt="" />
+        <img src="/public/images/categories/Beef.jpg" alt="" />
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
