a9c899b06ae87e28b15f4c65497e7ae3bc9d2c4b
Sofia Protsiv <prochivsofia@gmail.com>
2024-06-12 13:04:25 +0300

    feat(PathInfo): add breadcrumb logic and styles

--- src/components/PathInfo/PathInfo.jsx
+++ src/components/PathInfo/PathInfo.jsx
@@ -1,7 +1,35 @@
+import { Link, useLocation, useParams } from 'react-router-dom';
+import { useGetRecipeByIdQuery } from '../../redux/recipes/recipesApi';
 import cl from './pathInfo.module.scss';
 
 const PathInfo = () => {
-  return <div className={cl.className}>PathInfo</div>;
+  const location = useLocation();
+  const { recipeId } = useParams();
+  const { data: recipe } = useGetRecipeByIdQuery(recipeId);
+  const path = location.pathname;
+
+  const generateBreadcrumb = () => {
+    if (path === '/recipe/add') {
+      return 'Add recipe';
+    } else if (path.startsWith('/recipe/')) {
+      return recipe?.title;
+    } else if (path.startsWith('/user')) {
+      return 'Profile';
+    } else {
+      return '';
+    }
+  };
+  const isRecipeFound = path.startsWith('/recipe/') && recipe;
+  return (
+    <div className={cl.breadcrumb}>
+      <Link to="/" className={cl['home-link']}>
+        Home
+      </Link>
+      {isRecipeFound ? <span>/</span> : null}
+
+      <p className={cl.link}>{generateBreadcrumb()}</p>
+    </div>
+  );
 };
 
 export default PathInfo;
