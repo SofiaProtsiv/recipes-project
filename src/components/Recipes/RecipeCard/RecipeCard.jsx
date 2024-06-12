c201364f4108b43898c9682cd882628670107f5c
Artur Didur <didur.art@gmail.com>
2024-06-12 18:23:15 +0200

    feat recipe additional info

--- src/components/Recipes/RecipeCard/RecipeCard.jsx
+++ src/components/Recipes/RecipeCard/RecipeCard.jsx
@@ -1,17 +1,20 @@
+import RecipeExtra from './RecipeExtra';
 import cl from './recipeCard.module.scss';
 import PropTypes from 'prop-types';
 
-const RecipeCard = ({ recipe }) => {
+const RecipeCard = ({ recipe }, isFavorite = false) => {
+  const { thumb, title, instructions, owner } = recipe;
   return (
     <>
       <li className={cl.recipeItem}>
         <img
-          src={recipe.thumb}
-          alt={recipe.title}
+          src={thumb}
+          alt={title}
           className={`${cl.recipeImg} ${cl.skeleton}`}
         ></img>
-        <h3 className={cl.recipeTitle}>{recipe.title} </h3>
-        <p className={cl.recipeText}>{recipe.instructions}</p>
+        <h3 className={cl.recipeTitle}>{title} </h3>
+        <p className={cl.recipeText}>{instructions}</p>
+        <RecipeExtra owner={owner} isFavorite={isFavorite} />
       </li>
     </>
   );
@@ -22,7 +25,13 @@ RecipeCard.propTypes = {
     thumb: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     instructions: PropTypes.string.isRequired,
+    owner: PropTypes.shape({
+      _id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      avatar: PropTypes.string,
+    }),
   }).isRequired,
+  isFavorite: PropTypes.bool,
 };
 
 export default RecipeCard;
