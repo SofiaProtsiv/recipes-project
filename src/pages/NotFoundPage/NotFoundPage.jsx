32d8d18c1e22ff814b249a148569b9f723d7d564
M. A. Startsev <m.a.startsev@gmail.com>
2024-06-14 23:58:20 +0300

    added page 404 img+button

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,7 +1,26 @@
+import { Outlet } from 'react-router-dom';
+import { Suspense } from 'react';
+import Container from '../../components/ui/Container';
+import ButtonLink from '../../components/ui/ButtonLink';
+import image404 from '../../assets/icons/foodies404.svg';
 import cl from './notFoundPage.module.scss';
 
-const NotFoundPage = () => {
-  return <div className={cl.className}>NotFoundPage</div>;
-};
-
-export default NotFoundPage;
+export default function NotFoundPage() {
+  return (
+    <>
+      <Container addClass={cl.homepage}>
+        <Suspense fallback={<div>Loading main content...</div>}>
+          <Outlet />
+          <div className={cl.wrapper}>
+            <svg className={cl.foodies404} width={320} height={320}>
+              <use href={`${image404}#foodies404`}></use>
+            </svg>
+            <ButtonLink addClass={cl.to_home} to={'/'}>
+              Back to Home
+            </ButtonLink>
+          </div>
+        </Suspense>
+      </Container>
+    </>
+  );
+}
