0373f3aab9724f20199941bf0f6afd4a710dcad5
Yuriy-St <yuriystaynov@gmail.com>
2024-06-14 14:22:52 +0300

    refactor(common): enhance Container styling

--- src/components/ui/Container/Container.jsx
+++ src/components/ui/Container/Container.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import cl from './container.module.scss';
 
 const Container = ({ children, addClass = '' }) => {
-  return <div className={`${cl.container} ${addClass}`}>{children}</div>;
+  return <div className={`container ${addClass}`}>{children}</div>;
 };
 
 Container.propTypes = {
