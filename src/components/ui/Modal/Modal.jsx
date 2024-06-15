0ef3d2a76048294d496d45d431d372911440c18a
Vladyslav Mykhalytskyi <vmykhali666@gmail.com>
2024-06-15 23:13:04 +0300

    Revert "UI fix change hero layout, new images, fix login/logout userbar"
    This reverts commit 1df0aafdc829726126eff64ebe71ee0134b05886.
    
    # Conflicts:
    #	src/components/Hero/Hero.jsx

--- src/components/ui/Modal/Modal.jsx
+++ src/components/ui/Modal/Modal.jsx
@@ -17,8 +17,7 @@ const Modal = ({ onClose, type = 'LogOutModal' }) => {
   const [show, setShow] = useState(true);
   const modalRef = useRef(null);
 
-  // const { name } = useSelector(state => state.authSlice?.user);
-  const name = 'User Name';
+  const { name } = useSelector(state => state.authSlice?.user);
 
   const closeWithAnimation = () => {
     setShow(false);
