2c9a6b0b9f2a483b14aa8238e4a59904c328b7e6
Yuriy-St <yuriystaynov@gmail.com>
2024-06-17 18:06:43 +0300

    feat(userPage): page layout

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,12 +2,13 @@ import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import Container from '../components/ui/Container';
 
 export default function Layout() {
   return (
     <>
       <Header />
-      <Suspense fallback={<div>Loading main content...</div>}>
+      <Suspense fallback={<Container>Loading main content...</Container>}>
         <Outlet />
       </Suspense>
       <Footer />
